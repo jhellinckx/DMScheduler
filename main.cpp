1d10c8a5029f23aa78ccec2eb31491f279e896d1
acaccia <acaccia@ulb.ac.be>
2016-11-09 14:21:54 +0100

    [add] task

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "src/task.hpp"
+#include <iostream>
+
+int main() {
+  Task t(1, 2, 3, 4);
+  std::cout << t.o << std::endl;
+  return 0;
+}
---
+++ src/task.hpp
@@ -0,0 +1,10 @@
+#ifndef TASK_HPP
+#define TASK_HPP
+
+struct Task {
+  unsigned o, t, d, c;
+
+  Task(unsigned, unsigned, unsigned, unsigned);
+};
+
+#endif
