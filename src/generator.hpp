327de87a02fb9126b8195cd776235494b0aa68ee
acaccia <acaccia@ulb.ac.be>
2016-12-07 15:51:51 +0100

    Generator: hole in one

---
+++ src/generator.hpp
@@ -0,0 +1,26 @@
+#ifndef __GENERATOR_HPP
+#define __GENERATOR_HPP
+
+#include "task.hpp"
+#include <vector>
+
+class Generator {
+  double utilisation_goal;
+  int n;
+  std::vector<Task> tasks;
+
+public:
+  Generator() = default;
+  Generator(double u, int n);
+  std::string pprint() const;
+
+private:
+  void create_jobs();
+
+public:
+  bool utilisation_check() const;
+  bool ub_check() const;
+  double u() const;
+};
+
+#endif
