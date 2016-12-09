19e3d309470568a7ce68507872ef3aafccc6dccc
Jérôme Hellinckx <jeromehellinckx@hotmail.com>
2016-12-09 01:21:30 +0100

    [fix] Improved output format

--- src/prettify_schedule.py
+++ src/prettify_schedule.py
@@ -9,9 +9,10 @@ executions = 	[
 				]
 
 PROC_MARGIN = 0.2
-HEIGHT_BASE = 1
-WIDTH_BASE = 1
-TIME_UNITS_PER_LINE = 50;
+BASE_HEIGHT = 1
+BASE_WIDTH = 1
+
+TIME_UNITS_PER_LINE = 20;
 
 IDLE_EXEC = -1
 
@@ -32,13 +33,13 @@ class GraphicalSchedule(object):
 	def __init__(self, executions):
 		self.executions = executions
 		self.num_procs = len(self.executions)
-		self.line_height = self.num_procs * (HEIGHT_BASE + PROC_MARGIN) + HEIGHT_BASE
+		self.line_height = self.num_procs * (BASE_HEIGHT + PROC_MARGIN) + BASE_HEIGHT
 		self.needed_lines = int(math.ceil(len(max(self.executions, key = lambda x: len(x))) / float(TIME_UNITS_PER_LINE)))
 		self.figure = plt.figure()
-		self.ax = self.figure.add_subplot(111, aspect = "auto")
+		self.ax = self.figure.add_subplot(111, aspect = "equal")
 		self.ax.axis("off")
 		self.tot_height = self.line_height * self.needed_lines
-		self.tot_width = (TIME_UNITS_PER_LINE * WIDTH_BASE) + 2 * WIDTH_BASE
+		self.tot_width = (TIME_UNITS_PER_LINE * BASE_WIDTH) + 2 * BASE_WIDTH
 		self.ax.set_xlim([0, self.tot_width])
 		self.ax.set_ylim([0, self.tot_height])
 		self.current_line = 1
@@ -54,16 +55,16 @@ class GraphicalSchedule(object):
 	def draw_line(self):
 		height_offset = self.tot_height - (self.current_line * self.line_height)
 		self.draw_time_units_textboxes(height_offset)
-		height_offset += HEIGHT_BASE
+		height_offset += BASE_HEIGHT
 		for i in range (self.num_procs, 0, -1):
 			self.draw_proc_textbox(i, height_offset)
 			self.draw_proc_schedule(i - 1, height_offset)
-			height_offset += HEIGHT_BASE + PROC_MARGIN
+			height_offset += BASE_HEIGHT + PROC_MARGIN
 		self.current_line += 1
 
 	def draw_time_units_textboxes(self, height_offset):
 		for i in range(TIME_UNITS_PER_LINE):
-			self.centered_textbox(WIDTH_BASE * i + WIDTH_BASE, height_offset, str(self.line_starting_time() + i), fontsize = TIME_UNIT_FONT_SIZE)
+			self.centered_textbox(BASE_WIDTH * i + BASE_WIDTH, height_offset, str(self.line_starting_time() + i), fontsize = TIME_UNIT_FONT_SIZE)
 
 	def draw_proc_textbox(self, proc, height_offset):
 		self.centered_textbox(0, height_offset, "$" + PROC_SYMBOL + "_" + str(proc) + "$")
@@ -81,13 +82,13 @@ class GraphicalSchedule(object):
 				task_execs += 1
 			else:
 				if prev_task_id != IDLE_EXEC:
-					exec_rectangle = patches.Rectangle(((i - task_execs) * WIDTH_BASE + WIDTH_BASE, height_offset), task_execs * WIDTH_BASE, HEIGHT_BASE, fill = False, lw = EXEC_LINE_WIDTH)
+					exec_rectangle = patches.Rectangle(((i - task_execs) * BASE_WIDTH + BASE_WIDTH, height_offset), task_execs * BASE_WIDTH, BASE_HEIGHT, fill = False, lw = EXEC_LINE_WIDTH)
 					self.ax.add_patch(exec_rectangle)
 					self.annotate_rectangle(exec_rectangle, "$" + TASK_SYMBOL + "_" + str(prev_task_id) + "$")
 				task_execs = 1
 			prev_task_id = task_id
 		if task_id != IDLE_EXEC:
-			exec_rectangle = patches.Rectangle(((i - task_execs) * WIDTH_BASE + 2 * WIDTH_BASE, height_offset), task_execs * WIDTH_BASE, HEIGHT_BASE, fill = False, lw = EXEC_LINE_WIDTH)
+			exec_rectangle = patches.Rectangle(((i - task_execs) * BASE_WIDTH + 2 * BASE_WIDTH, height_offset), task_execs * BASE_WIDTH, BASE_HEIGHT, fill = False, lw = EXEC_LINE_WIDTH)
 			self.ax.add_patch(exec_rectangle)
 			self.annotate_rectangle(exec_rectangle, "$" + TASK_SYMBOL + "_" + str(task_id) + "$")
 
@@ -101,7 +102,7 @@ class GraphicalSchedule(object):
 		self.ax.annotate(text, (cx, cy), **dict(default_args, **kwargs))
 
 	def centered_textbox(self, x, y, text, **kwargs):
-		r = patches.Rectangle((x, y), WIDTH_BASE, HEIGHT_BASE)
+		r = patches.Rectangle((x, y), BASE_WIDTH, BASE_HEIGHT)
 		self.annotate_rectangle(r, text, **kwargs)
 
 	def save(self):
