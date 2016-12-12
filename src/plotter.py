4640aeb6dabc48cdab0699459c2239d1969144ad
acaccia <acaccia@ulb.ac.be>
2016-12-12 17:21:10 +0100

    plotter now with two ys

--- src/plotter.py
+++ src/plotter.py
@@ -6,14 +6,27 @@ from ast import literal_eval
 import matplotlib.pyplot as plt
 
 XS = literal_eval(sys.argv[1])
-YS = literal_eval(sys.argv[2])
-X_NAME = sys.argv[3]
-Y_NAME = sys.argv[4]
-OUTPUT_FILENAME = sys.argv[5]
+YS1 = literal_eval(sys.argv[2])
+YS2 = literal_eval(sys.argv[3])
+X_NAME = sys.argv[4]
+Y1_NAME = sys.argv[5]
+Y2_NAME = sys.argv[6]
+OUTPUT_FILENAME = sys.argv[7]
 
-xs, ys = zip(*[(x, y) for x, y in zip(XS, YS) if y != 'nan'])
+xs1, ys1 = zip(*[(x, y) for x, y in zip(XS, YS1) if y != 'nan'])
+xs2, ys2 = zip(*[(x, y) for x, y in zip(XS, YS2) if y != 'nan'])
 
-plt.plot(xs, ys)
-plt.xlabel(X_NAME)
-plt.ylabel(Y_NAME)
+fig, ax1 = plt.subplots()
+ax1.plot(xs1, ys1, "b-")
+ax1.set_xlabel(X_NAME)
+ax1.set_ylabel(Y1_NAME, color="b")
+for tl in ax1.get_yticklabels():
+    tl.set_color('b')
+
+ax2 = ax1.twinx()
+ax2.plot(xs2, ys2, "r-")
+ax2.set_ylabel(Y2_NAME, color="r")
+for tl in ax2.get_yticklabels():
+    tl.set_color('r')
+
 plt.savefig(OUTPUT_FILENAME)
