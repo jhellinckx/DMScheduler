ff0f3bb9bd6b4072cb1abd444f26dab0a917cdc2
acaccia <acaccia@ulb.ac.be>
2016-12-12 17:47:28 +0100

    better naming in plotter

--- src/plotter.py
+++ src/plotter.py
@@ -10,8 +10,8 @@ YS1 = literal_eval(sys.argv[2])
 YS2 = literal_eval(sys.argv[3])
 X_NAME = sys.argv[4]
 Y_NAME = sys.argv[5]
-Y1_NAME = sys.argv[6]
-Y2_NAME = sys.argv[7]
+Y1_LABEL = sys.argv[6]
+Y2_LABEL = sys.argv[7]
 OUTPUT_FILENAME = sys.argv[8]
 
 LEGEND_LOCATION = "best"
@@ -19,8 +19,8 @@ LEGEND_LOCATION = "best"
 xs1, ys1 = zip(*[(x, y) for x, y in zip(XS, YS1) if y != 'nan'])
 xs2, ys2 = zip(*[(x, y) for x, y in zip(XS, YS2) if y != 'nan'])
 
-plt.plot(xs1, ys1, "b-", label=Y1_NAME)
-plt.plot(xs2, ys2, "b-", label=Y2_NAME)
+plt.plot(xs1, ys1, "b-", label=Y1_LABEL)
+plt.plot(xs2, ys2, "b-", label=Y2_LABEL)
 plt.xlabel(X_NAME)
 plt.ylabel(Y_NAME)
 plt.legend(loc=LEGEND_LOCATION)
